3dde3643b35da1682979f4c480f18751db586c19
ytakahashi <aqxsws@gmail.com>
2016-08-29 11:28:39 +0900

    zsh

--- symlink.sh
+++ symlink.sh
@@ -5,4 +5,5 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vim/colors ~/.vim
-
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
