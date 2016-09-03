0211d2f75d90b34e815f4b4ed15e1dd203849ba5
ytakahashi <aqxsws@gmail.com>
2016-09-03 14:36:00 +0900

    devide mac and linux

--- symlink.sh
+++ symlink.sh
@@ -2,8 +2,12 @@
 
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vim/colors ~/.vim
+
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
+
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.tmux.d/ ~/.tmux.d
