4ff7e51d78973ec93b61a52206ecce19ec538206
ytakahashi <aqxsws@gmail.com>
2018-04-16 22:01:34 +0900

    Add setup script
    - setup script
    - zplug, fzf

--- lib/color.sh
+++ lib/color.sh
@@ -2,7 +2,12 @@
 
 readonly GREEN=$(tput setaf 2)
 readonly YELLOW=$(tput setaf 3)
+readonly CYAN=$(tput setaf 6)
 readonly NORMAL=$(tput sgr0)
+
+function info() {
+  printf "%s\n" "$CYAN$1$NORMAL"
+}
 
 function ok() {
   printf "%s\n" "$GREEN$1$NORMAL"
