b0673055dd4d172a67246efd0b3265ef7060434a
ytakahashi <yt01tc@gmail.com>
2019-07-25 20:54:57 +0900

    added aliases

--- zsh/miscs.zsh
+++ zsh/miscs.zsh
@@ -7,6 +7,24 @@ _cdup() {
 zle -N _cdup
 bindkey '^T' _cdup
 
+_git_fethc_p() {
+  echo "git fetch -p"
+  git fetch -p
+  zle reset-prompt
+}
+
+zle -N _git_fethc_p
+bindkey '^G^F' _git_fethc_p
+
+_git_status_uall() {
+  echo "git status -uall"
+  git status -uall
+  zle reset-prompt
+}
+
+zle -N _git_status_uall
+bindkey '^G^G' _git_status_uall
+
 precmd_func() {
   print -Pn "\e]0;${${PWD}##*/}\a"
 }
