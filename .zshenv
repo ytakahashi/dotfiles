7ae0026468889278ca2575912280c925a11c45ef
ytakahashi <yt01tc@gmail.com>
2020-05-15 15:04:20 +0900

    fix deno

--- .zshenv
+++ .zshenv
@@ -5,7 +5,4 @@ export PATH=/usr/local/bin:$PATH
 
 export PATH="$HOME/bin:$PATH"
 
-export DENO_INSTALL=$HOME/.deno
-export PATH=$DENO_INSTALL/bin:$PATH
-
 export PIPENV_VENV_IN_PROJECT=true
--- zsh/languages.zsh
+++ zsh/languages.zsh
@@ -26,3 +26,7 @@ export SDKMAN_DIR="$HOME/.sdkman"
 if [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]]; then
   source "$SDKMAN_DIR/bin/sdkman-init.sh"
 fi
+
+# Deno
+export DENO_INSTALL=$HOME/.deno
+export PATH=$DENO_INSTALL/bin:$PATH
