b8f79514df62c325e97436cd103055e4a33e69cc
ytakahashi <yt01tc@gmail.com>
2025-03-29 18:02:53 +0900

    add mise

--- zsh/languages.zsh
+++ zsh/languages.zsh
@@ -24,6 +24,11 @@ elif type anyenv > /dev/null 2>&1; then
   source ~/.anyenv-init.sh
 fi
 
+# mise
+if type mise > /dev/null 2>&1; then
+  eval "$(mise activate zsh)"
+fi
+
 # *envs
 export PATH=$PATH:$GOPATH/bin
 
